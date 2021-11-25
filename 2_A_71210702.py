8149a1a557146d9b21258422db6f9a5d8400b780
Farrel-Saputro <farrel.ardiyanto@ti.ukdw.ac.id>
2021-11-25 17:16:10 +0700

    2_A_71210702

---
+++ 2_A_71210702.py
@@ -0,0 +1,14 @@
+def ambil_tengah(huruf, tambah=0):
+    if(tambah):
+        hitung = len(huruf)
+        bagi = (hitung//2)
+        return(huruf[bagi+tambah])
+    else:
+        hitung = len(huruf)
+        bagi = (hitung//2)
+        return(huruf[bagi])
+
+
+print(ambil_tengah("abcdefg", 1))
+print(ambil_tengah("abcdefg", 2))
+print(ambil_tengah("abcd"))
